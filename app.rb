c4074cd771bdf8d107c2ab66dc1e95b32905efbd
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-25 00:29:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,4 +15,19 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     @number.to_i.times.collect {@phrase}.join(' ')
   end
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    case @operation
+    when "add"
+      "#{@number1+@number2}"
+    when "subtract"
+      "#{@number1-@number2}"
+    when "multiply"
+      "#{@number1*@number2}"
+    when "divide"
+      "#{@number1/@number2}"
+    end
+  end
 end
