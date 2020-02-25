8b3f248f4b29bf88690848d211d70a72c171b12d
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-25 00:37:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     binding.pry
     case @operation
     when "add"
