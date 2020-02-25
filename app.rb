2ab75f98645abcb23ccb4dd934bbfe46f564fea7
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-25 00:32:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
   get '/square/:number' do
     @number = params[:number].to_i
-    @number.square.to_s
+    "#{@number*@number}"
   end
   get '/say/:number/:phrase' do
     @number = params[:number]
